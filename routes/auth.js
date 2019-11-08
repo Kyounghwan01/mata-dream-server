c0f6aa2d4562a7ccea2442c381e3f5a88efeeee1
kyounghwan <osc9245@naver.com>
2019-11-08 15:56:49 +0900

    add get seller data router

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,14 @@ const User = require('../models/User');
 router.post('/login/facebook', authController.findOrCreateUser, authController.createToken);
 router.post('/logout', authController.logout);
 
-router.get('/user', async (req, res, next) => {
+router.get('/user', authController.verifyToken, async (req, res, next) => {
   const user = await User.findOne({social_id : req.session.socialId});
-  console.log(user);
-  console.log(req.session);
   res.send({result : user});
+})
+
+router.get('/seller/:id', authController.verifyToken, async (req, res, next) => {
+  const seller = await User.findOne().where('_id').equals(req.params.id);
+  res.send({result : seller});
 })
 
 module.exports = router;
