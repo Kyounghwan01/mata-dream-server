6fa35d8b4b8466a34ee91e8407cc6e1d58ada06c
kyounghwan <osc9245@naver.com>
2019-11-15 17:44:04 +0900

    add server test

---
+++ spec/test.js
@@ -0,0 +1,45 @@
+const request = require("supertest");
+const { expect } = require("chai");
+const jwt = require("jsonwebtoken");
+// const USERS = require('../routes/users').USERS;
+
+const app = require("../app");
+
+const userData = {
+  socialService: 'FACEBOOK',
+  socialId: 112034056905615,
+  userName: 'asd',
+  profileImage: 'image',
+  socialToken: 'fbToken'
+};
+
+describe("POST login data ", () => {
+  it("should not allow no have token user", done => {
+    request(app)
+      .get("/auth/user")
+      .expect("Content-Type", /json/)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res.body).to.eql({ error: "unauthorized" });
+        done();
+      });
+  });
+
+  it("should generate token to valiud user", done => {
+    request(app)
+      .post("/auth/login/facebook")
+      .send({...userData})
+      .end((err, res) => {
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        console.log(res.headers.usertoken);
+        expect(Boolean(res.headers.usertoken)).to.equal(true);
+        done();
+      })
+  });
+
+});
