2c5f7e4f3f1ef56d88a6da3665a28a503eb7854f
kyounghwan <osc9245@naver.com>
2019-11-15 20:21:02 +0900

    modified test

--- spec/test.js
+++ spec/test.js
@@ -1,45 +1,45 @@
-const request = require("supertest");
-const { expect } = require("chai");
-const jwt = require("jsonwebtoken");
+// const request = require("supertest");
+// const { expect } = require("chai");
+// const jwt = require("jsonwebtoken");
 
-const app = require("../app");
+// const app = require("../app");
 
-const userData = {
-  socialService: 'FACEBOOK',
-  socialId: 112034056905615,
-  userName: 'asd',
-  profileImage: 'image',
-  socialToken: 'fbToken'
-};
+// const userData = {
+//   socialService: 'FACEBOOK',
+//   socialId: 112034056905615,
+//   userName: 'asd',
+//   profileImage: 'image',
+//   socialToken: 'fbToken'
+// };
 
-describe("POST login data ", () => {
-  xit("should not allow no have token user", done => {
-    request(app)
-      .get("/auth/user")
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res.body).to.eql({ error: "unauthorized" });
-        done();
-      });
-  });
+// describe("POST login data ", () => {
+//   xit("should not allow no have token user", done => {
+//     request(app)
+//       .get("/auth/user")
+//       .expect("Content-Type", /json/)
+//       .end((err, res) => {
+//         if (err) {
+//           return done(err);
+//         }
+//         expect(res.body).to.eql({ error: "unauthorized" });
+//         done();
+//       });
+//   });
 
-  xit("should generate token to valiud user", done => {
-    request(app)
-      .post("/auth/login/facebook")
-      .send({...userData})
-      .end((err, res) => {
-        if(err){
-          console.log(err);
-          return done(err);
-        }
-        if(res){
-          expect(Boolean(res.headers.usertoken)).to.equal(true);
-          done();
-        }
-      })
-  });
+//   xit("should generate token to valiud user", done => {
+//     request(app)
+//       .post("/auth/login/facebook")
+//       .send({...userData})
+//       .end((err, res) => {
+//         if(err){
+//           console.log(err);
+//           return done(err);
+//         }
+//         if(res){
+//           expect(Boolean(res.headers.usertoken)).to.equal(true);
+//           done();
+//         }
+//       })
+//   });
 
-});
+// });
